69a19d7f45e875f9b34c3e01e74dd29f5e2a73aa
dfrancoce <danielfrancocecilia@gmail.com>
2014-06-05 20:08:13 +0200

    DDL and DML scripts

---
+++ db_scripts/ddl_taskerDB.sql
@@ -0,0 +1,133 @@
+﻿-- BOARDS
+-- DROP TABLE "Boards";
+CREATE TABLE "Boards"
+(
+  id serial NOT NULL,
+  code text,
+  name text,
+  CONSTRAINT boards_pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Boards"
+  OWNER TO postgres;
+
+-- PRIORITIES
+-- DROP TABLE "Priorities";
+CREATE TABLE "Priorities"
+(
+  id serial NOT NULL,
+  name text,
+  CONSTRAINT priorities_pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Priorities"
+  OWNER TO postgres;
+
+-- PROJECTS
+-- DROP TABLE "Projects";
+CREATE TABLE "Projects"
+(
+  id serial NOT NULL,
+  code text,
+  name text,
+  CONSTRAINT projects_pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Projects"
+  OWNER TO postgres;
+
+-- STATES
+-- DROP TABLE "States";
+CREATE TABLE "States"
+(
+  id serial NOT NULL,
+  name text,
+  CONSTRAINT states_pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "States"
+  OWNER TO postgres;
+
+-- TYPES
+-- DROP TABLE "Types";
+CREATE TABLE "Types"
+(
+  id serial NOT NULL,
+  name text,
+  CONSTRAINT types_pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Types"
+  OWNER TO postgres;
+
+-- USERS
+-- DROP TABLE "Users";
+CREATE TABLE "Users"
+(
+  id serial NOT NULL,
+  code text,
+  name text,
+  CONSTRAINT users_pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Users"
+  OWNER TO postgres;
+
+-- TASKS
+-- DROP TABLE "Tasks";
+CREATE TABLE "Tasks"
+(
+  id serial NOT NULL,
+  code text,
+  name text,
+  description text,
+  board smallint,
+  state smallint,
+  project smallint,
+  priority smallint,
+  type smallint,
+  estimation smallint,
+  incurred smallint,
+  assignedto smallint,
+  CONSTRAINT tasks_pk_id PRIMARY KEY (id),
+  CONSTRAINT task_types_fk FOREIGN KEY (type)
+      REFERENCES "Types" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT tasks_boards_fk FOREIGN KEY (board)
+      REFERENCES "Boards" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT tasks_priorities_fk FOREIGN KEY (priority)
+      REFERENCES "Priorities" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT tasks_projects_fk FOREIGN KEY (project)
+      REFERENCES "Projects" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT tasks_states_fk FOREIGN KEY (state)
+      REFERENCES "States" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT tasks_users_fk FOREIGN KEY (assignedto)
+      REFERENCES "Users" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Tasks"
+  OWNER TO postgres;
+
+
+
+
+
---
+++ db_scripts/dml_taskerDB.sql
@@ -0,0 +1,90 @@
+﻿-- BOARDS
+INSERT INTO "Boards"(code, name) VALUES ('BOARD1', 'Tboard1');
+INSERT INTO "Boards"(code, name) VALUES ('BOARD2', 'Tboard2');
+INSERT INTO "Boards"(code, name) VALUES ('BOARD3', 'Tboard3');
+
+SELECT * FROM "Boards";
+
+-- PROJECTS
+INSERT INTO "Projects"(code, name) VALUES ('PROJ1', 'Tproject1');
+INSERT INTO "Projects"(code, name) VALUES ('PROJ2', 'Tproject2');
+INSERT INTO "Projects"(code, name) VALUES ('PROJ3', 'Tproject3');
+
+SELECT * FROM "Projects";
+
+-- PRIORITIES
+INSERT INTO "Priorities"(name) VALUES ('Low');
+INSERT INTO "Priorities"(name) VALUES ('Medium');
+INSERT INTO "Priorities"(name) VALUES ('High');
+INSERT INTO "Priorities"(name) VALUES ('Very High');
+INSERT INTO "Priorities"(name) VALUES ('DEFCON 1');
+
+SELECT * FROM "Priorities";
+
+-- STATES
+INSERT INTO "States"(name) VALUES ('To do');
+INSERT INTO "States"(name) VALUES ('In progress');
+INSERT INTO "States"(name) VALUES ('Finished');
+
+SELECT * FROM "States";
+
+-- TYPES
+INSERT INTO "Types"(name) VALUES ('Design');
+INSERT INTO "Types"(name) VALUES ('Development');
+INSERT INTO "Types"(name) VALUES ('Testing');
+INSERT INTO "Types"(name) VALUES ('Documentation');
+
+SELECT * FROM "Types";
+
+-- USERS
+INSERT INTO "Users"(code, name) VALUES ('USER1', 'Tuser1');
+INSERT INTO "Users"(code, name) VALUES ('USER2', 'Tuser2');
+INSERT INTO "Users"(code, name) VALUES ('USER3', 'Tuser3');
+
+SELECT * FROM "Users";
+
+-- TASKS
+INSERT INTO "Tasks"(
+	code, 
+	name, 
+	description, 
+	board, 
+	state, 
+	project, 
+	priority, 
+	type, 
+	estimation, 
+	incurred, 
+	assignedTo
+) VALUES ('TASK1', 'Test task 1', 'This is a task to test the app', 1, 1, 1, 1, 1, 0, 0, 1);
+
+INSERT INTO "Tasks"(
+	code, 
+	name, 
+	description, 
+	board, 
+	state, 
+	project, 
+	priority, 
+	type, 
+	estimation, 
+	incurred, 
+	assignedTo
+) VALUES ('TASK2', 'Test task 2', 'This is a task to test the app', 2, 2, 2, 2, 2, 0, 0, 2);
+
+INSERT INTO "Tasks"(
+	code, 
+	name, 
+	description, 
+	board, 
+	state, 
+	project, 
+	priority, 
+	type, 
+	estimation, 
+	incurred, 
+	assignedTo
+) VALUES ('TASK3', 'Test task 3', 'This is a task to test the app', 3, 3, 3, 3, 3, 0, 0, 3);
+
+SELECT * FROM "Tasks";
+
